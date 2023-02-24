5e35fd5a4bb4b6144f345f5e5298a79ddf324664
Leonardo Leal <117487925+Leozhr@users.noreply.github.com>
2023-02-24 11:43:02 -0300

    Configuração Inicial & Componentes de Login

---
+++ src/app/pages/login/login.routes.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login.component';
+
+const LoginRouter: Routes = [{ path: '', component: LoginComponent }];
+
+@NgModule({
+  imports: [RouterModule.forChild(LoginRouter)],
+  exports: [RouterModule],
+})
+export class MovieRoutingModule {}
